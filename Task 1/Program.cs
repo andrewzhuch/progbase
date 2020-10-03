f40d3d66104fc6fc44957d055dd659ae86cce162
Andrew Zhuchenko <dron@Andrews-MacBook-Pro.local>
2020-10-03 13:14:45 +0300

    task 1 done

---
+++ Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I'm Andrew Zhuchenko");
+            WriteLine("I'm from KP-02");
+            // Python is better...
+        }
+    }
+}
